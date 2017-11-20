1ee2b3c7484bda1863c6436d837ab8ca1448cd1b
m4dz <code@m4dz.net>
2017-11-20 22:57:00 +0100

    :shirt: Rename app.js to server.js
    The project is a server, right? So the main entry point need to be
    renamed accordingly.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var notify = require('gulp-notify')
 var clean = require('gulp-clean')
 
 gulp.task('es6', () => {
-  return gulp.src(['app.js', 'routes/*', 'lib/*', 'middleware/*'], {base: '.'})
+  return gulp.src(['server.js', 'routes/*', 'lib/*', 'middleware/*'], {base: '.'})
         .pipe(babel({presets: ['es2015']}))
         .pipe(gulp.dest('dist'))
 })
@@ -22,12 +22,12 @@ gulp.task('clean', function () {
 
 gulp.task('server', function () {
   nodemon({
-    script: 'dist/app.js',
-    watch: ['app.js', 'routes/*', 'lib/*', 'middleware/*', 'config/*'],
+    script: 'dist/server.js',
+    watch: ['server.js', 'routes/*', 'lib/*', 'middleware/*', 'config/*'],
     ext: 'js json',
     tasks: ['build']
   }).on('restart', function () {
-    gulp.src('app.js').pipe(notify('Server successfully restarted'))
+    gulp.src('server.js').pipe(notify('Server successfully restarted'))
   })
 })
 
