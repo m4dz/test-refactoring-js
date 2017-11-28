4df8c0f811d02bfcad0a000bd86ccd8c914d9ee7
m4dz <code@m4dz.net>
2017-11-28 11:27:03 +0100

    :hammer: Re-enable products routes
    Re-enable products routes with a Promise-wise DB, which ensure a
    better reponsiveness for incoming requests.

--- routes/products.js
+++ routes/products.js
@@ -1,20 +1,45 @@
 'use strict'
 
+import path from 'path'
 import express from 'express'
+import sqlite from 'sqlite'
 import { isAuth } from '../lib/middleware'
 
+// Path is relative to the `dist` directory
+const DB_PATH = path.resolve(__dirname, '../../data/database.sqlite')
+
 const router = express.Router()
+const dbPromise = sqlite.open(DB_PATH, { Promise })
 
-router.get('/list', (req, res) => {
-  res.send('products list')
+router.get('/list', async (req, res, next) => {
+  try {
+    const db = await dbPromise
+    const products = await db.all('SELECT * FROM products')
+    res.render('list', { products })
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/view/:id', isAuth, (req, res) => {
-  res.send(`products view ${req.params.id}`)
+router.get('/view/:id', isAuth, async (req, res, next) => {
+  try {
+    const db = await dbPromise
+    const product = await db.get('SELECT * FROM products WHERE id = ?', req.params.id)
+    res.render('view', { product })
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/cart/:id', isAuth, (req, res) => {
-  res.send(`products cart ${req.params.id}`)
+router.get('/cart/:id', isAuth, async (req, res, next) => {
+  try {
+    const db = await dbPromise
+    const product = await db.get('SELECT * FROM products WHERE id = ?', req.params.id)
+    console.log(product)
+    res.json({success: true, text: `Product ${req.params.id} successfully bought`})
+  } catch (err) {
+    next(err)
+  }
 })
 
 // var express = require('express')
