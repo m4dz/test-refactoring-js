ec50a12babb4ad081f7a95fe40be95057a92b92c
m4dz <code@m4dz.net>
2017-11-28 11:58:35 +0100

    :hammer: Move db logics outside of routing
    We now expose a db wrapper to access products data, outside of the
    routing component.

--- routes/products.js
+++ routes/products.js
@@ -1,23 +1,16 @@
 'use strict'
 
-import path from 'path'
 import express from 'express'
-import sqlite from 'sqlite'
 import { isAuth } from '../lib/middleware'
-
-// Path is relative to the `dist` directory
-const DB_PATH = path.resolve(__dirname, '../../data/database.sqlite')
+import * as products from '../lib/products'
 
 const router = express.Router()
-const dbPromise = sqlite.open(DB_PATH, { Promise })
 
 router.get('/', (req, res) => res.redirect(301, '/products/list'))
 
 router.get('/list', async (req, res, next) => {
   try {
-    const db = await dbPromise
-    const products = await db.all('SELECT * FROM products')
-    res.render('list', { products })
+    res.render('list', { products: await products.all() })
   } catch (err) {
     next(err)
   }
@@ -25,8 +18,7 @@ router.get('/list', async (req, res, next) => {
 
 router.get('/view/:id', isAuth, async (req, res, next) => {
   try {
-    const db = await dbPromise
-    const product = await db.get('SELECT * FROM products WHERE id = ?', req.params.id)
+    const product = await products.get(req.params.id)
     res.render('view', { product })
   } catch (err) {
     next(err)
@@ -35,8 +27,7 @@ router.get('/view/:id', isAuth, async (req, res, next) => {
 
 router.get('/cart/:id', isAuth, async (req, res, next) => {
   try {
-    const db = await dbPromise
-    const product = await db.get('SELECT * FROM products WHERE id = ?', req.params.id)
+    const product = await products.get(req.params.id)
     console.log(product)
     res.json({success: true, text: `Product ${req.params.id} successfully bought`})
   } catch (err) {
